f87c55ab67e39fa2585ec725fdb046a59a4295ff
Boyce Lig <532762848@qq.com>
2025-12-09 21:01:13 +0800

    修改Build不成功问题，增加volumes

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,6 +3,9 @@
 echo "Downloading subscribe file..."
 curl -s -H "User-Agent: clash.meta" -o /etc/mihomo/config.yaml "${SUBSCRIBE_URL}"
 
+# 强制修改 external-controller 监听地址为 0.0.0.0
+sed -i "s/external-controller: '127.0.0.1/external-controller: '0.0.0.0/g" /etc/mihomo/config.yaml
+
 # 启动 mihomo（后台运行）
 echo "Starting mihomo..."
 /usr/local/bin/mihomo -d /etc/mihomo 2>&1 | tee /var/log/mihomo.log &
